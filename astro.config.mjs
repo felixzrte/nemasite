3a82e0c67948acb1e304f5c900c26d9196170d2c
Felix <felixzarate14@gmail.com>
2023-04-23 23:19:47 -0400

    vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
+import vercelStatic from '@astrojs/vercel/static'
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercelStatic(),
   integrations: [tailwind()]
 });
