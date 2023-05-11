d7da61175493d5a205ce76950a208c2cd30626bf
Felix <felixzarate14@gmail.com>
2023-05-11 14:46:12 -0400

    add analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
-
+import vercel from '@astrojs/vercel/static'
 // https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  adapter: vercel({
+    analytics: true
+  }),
 });
