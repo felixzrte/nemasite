c85e532724f87b4134d97f0868adc172d3d70b2d
Felix <felixzarate14@gmail.com>
2023-06-23 11:53:06 -0400

    edit: add new section to resources page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,19 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel/static'
+import vercel from "@astrojs/vercel/static";
 // https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   adapter: vercel({
-    analytics: true
+    analytics: true,
   }),
+  vite: {
+    ssr: {
+      noExternal: ["path-to-regexp"],
+    },
+  },
 });
